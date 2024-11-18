52e0b49bb097f113d10ee9907fb07760d55059b5
David Ruiz Peña <119055850+Jrmx21@users.noreply.github.com>
2024-11-18 20:26:31 +0100

    hace el rebote bien me parese

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -22,7 +22,7 @@ public class Ball : MonoBehaviour
 
     public void direccionAleatoria()
     {
-        _direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-0.5f, 0.5f)).normalized;
+        _direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
         _rigidBody2D = GetComponent<Rigidbody2D>();
         _rigidBody2D.velocity = _direction * _speed;
         _velocityPrev = _rigidBody2D.velocity;
@@ -43,25 +43,45 @@ public class Ball : MonoBehaviour
     {
         if (col.gameObject.CompareTag("Player"))
         {
-             lastRelativePosition = this.transform.position - col.transform.position;
-            Debug.Log("relativa" + lastRelativePosition.y);
+            lastRelativePosition = this.transform.position - col.transform.position;
             contactWithPlayer = col.contacts[0].point;
+            Vector2 newDirection = new Vector2(2, 0).normalized;
             //avisame cuando choca en la parte superior, en medio o inferior
-            if (lastRelativePosition.y > 0.5f)
+            if (lastRelativePosition.y > 0.4f)
             {
-                Debug.Log("choca arriba");
+                Debug.Log("Choca arriba");
+
+                // Obtienes la velocidad previa y la normalizas
+                Vector2 v = _velocityPrev.normalized;
+
+                // Cambio de dirección, por ejemplo, refleja el componente Y
+                _rigidBody2D.velocity = new Vector2(_velocityPrev.x, _velocityPrev.y * 0.5f); // Modifica como desees la reflexión
+                // Si lo deseas más complejo, puedes combinar la normal de la superficie de colisión con la velocidad.
+                Vector2 normal = col.contacts[0].normal;
+                _rigidBody2D.velocity = Vector2.Reflect(_velocityPrev, normal);
+                //dibuja el angulo que deberia ir normalmente
+                Debug.DrawRay(col.contacts[0].point, col.contacts[0].normal, Color.red, 2f);
+                
             }
-            else if (lastRelativePosition.y < -0.5f)
+            else if (lastRelativePosition.y < -0.4f)
             {
-                Debug.Log("choca abajo");
+                Debug.Log("Choca abajo");
+                // Similar para el caso de impacto abajo
+                //idibuja la normal
+                Debug.DrawRay(col.contacts[0].point, col.contacts[0].normal, Color.red, 2f);
+                Vector2 v = _velocityPrev.normalized;
+                _rigidBody2D.velocity = new Vector2(-_velocityPrev.x, _velocityPrev.y * 0.5f);
+                Vector2 normal = col.contacts[0].normal;
+                _rigidBody2D.velocity = Vector2.Reflect(_velocityPrev, normal);
             }
-            else
+            if (lastRelativePosition.y > -0.5f && lastRelativePosition.y < 0.5f)
             {
                 Debug.Log("choca en medio");
+                Vector2 v = _velocityPrev.normalized;
+                _rigidBody2D.velocity = new Vector2(v.x * _speedIncrement, v.y * _speedIncrement);
+                _rigidBody2D.velocity = new Vector2(-(_velocityPrev.x + v.x), _velocityPrev.y + v.y);
             }
-            Vector2 v = _velocityPrev.normalized;
-            _rigidBody2D.velocity = new Vector2(v.x * _speedIncrement, v.y * _speedIncrement);
-            _rigidBody2D.velocity = new Vector2(-(_velocityPrev.x + v.x), _velocityPrev.y + v.y);
+
         }
         else if (col.gameObject.CompareTag("Wall"))
         {
