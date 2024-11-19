7944941c020439220c82d2cea9c1c1636eefb60c
David Ruiz Peña <119055850+Jrmx21@users.noreply.github.com>
2024-11-19 20:54:58 +0100

    detecta bien angulos

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,7 +8,7 @@ public class Ball : MonoBehaviour
 {
     private Rigidbody2D _rigidBody2D;
     [SerializeField] private Vector2 _direction;
-    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _speed = 5f;
     [SerializeField] private float _speedIncrement = 0.1f;
     [SerializeField] private float _umbralX = 3f;
     [SerializeField] private Vector2 contactWithPlayer;
@@ -47,32 +47,38 @@ public class Ball : MonoBehaviour
             contactWithPlayer = col.contacts[0].point;
             Vector2 newDirection = new Vector2(2, 0).normalized;
             //avisame cuando choca en la parte superior, en medio o inferior
-            if (lastRelativePosition.y > 0.4f)
+            if (lastRelativePosition.y > 0.5f)
             {
                 Debug.Log("Choca arriba");
-
-                // Obtienes la velocidad previa y la normalizas
                 Vector2 v = _velocityPrev.normalized;
-
-                // Cambio de dirección, por ejemplo, refleja el componente Y
-                _rigidBody2D.velocity = new Vector2(_velocityPrev.x, _velocityPrev.y * 0.5f); // Modifica como desees la reflexión
-                // Si lo deseas más complejo, puedes combinar la normal de la superficie de colisión con la velocidad.
                 Vector2 normal = col.contacts[0].normal;
+                _rigidBody2D.velocity = new Vector2(_velocityPrev.x, _velocityPrev.y);
                 _rigidBody2D.velocity = Vector2.Reflect(_velocityPrev, normal);
-                //dibuja el angulo que deberia ir normalmente
-                Debug.DrawRay(col.contacts[0].point, col.contacts[0].normal, Color.red, 2f);
-                
+
+     
+                var angle = Vector2.Angle( _rigidBody2D.velocity, normal) * 2;
+
+                Debug.Log("original angle: " + Vector2.Angle(_rigidBody2D.velocity, normal));
+                Debug.Log("angle: " + angle);
+                // calcula el vector de reflexion con el angulo nuevo y establece la velocidad a la bola
+                var newDirectionAngle = Quaternion.AngleAxis(angle, Vector3.forward) * _velocityPrev;
+
             }
-            else if (lastRelativePosition.y < -0.4f)
+            if (lastRelativePosition.y < -0.5f)
             {
                 Debug.Log("Choca abajo");
                 // Similar para el caso de impacto abajo
-                //idibuja la normal
-                Debug.DrawRay(col.contacts[0].point, col.contacts[0].normal, Color.red, 2f);
                 Vector2 v = _velocityPrev.normalized;
-                _rigidBody2D.velocity = new Vector2(-_velocityPrev.x, _velocityPrev.y * 0.5f);
+                _rigidBody2D.velocity = new Vector2(_velocityPrev.x, _velocityPrev.y);
+
                 Vector2 normal = col.contacts[0].normal;
-                _rigidBody2D.velocity = Vector2.Reflect(_velocityPrev, normal);
+                // angle dividido por 2
+                var angle = Vector2.Angle(_velocityPrev, normal) / 2;
+                // calcula el vector de reflexion con el angulo nuevo y establece la velocidad a la bola
+                var newDirectionAngle = Quaternion.AngleAxis(angle, Vector3.forward) * _velocityPrev;
+                _rigidBody2D.velocity = Vector2.Reflect(newDirectionAngle, normal);
+
+
             }
             if (lastRelativePosition.y > -0.5f && lastRelativePosition.y < 0.5f)
             {
@@ -80,7 +86,11 @@ public class Ball : MonoBehaviour
                 Vector2 v = _velocityPrev.normalized;
                 _rigidBody2D.velocity = new Vector2(v.x * _speedIncrement, v.y * _speedIncrement);
                 _rigidBody2D.velocity = new Vector2(-(_velocityPrev.x + v.x), _velocityPrev.y + v.y);
+
             }
+            Debug.DrawRay(col.contacts[0].point, col.contacts[0].normal, Color.yellow, 2f);
+            Debug.DrawRay(col.contacts[0].point, -_velocityPrev, Color.red, 2f);
+            Debug.DrawRay(col.contacts[0].point, _rigidBody2D.velocity, Color.blue, 2f);
 
         }
         else if (col.gameObject.CompareTag("Wall"))
@@ -93,10 +103,6 @@ public class Ball : MonoBehaviour
             _rigidBody2D.velocity = new Vector2(_umbralX, _rigidBody2D.velocity.y);
             Debug.Log("Velocidad en x menor al umbral");
         }
-    }
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawSphere(contactWithPlayer, 0.1f);
+
     }
 }
